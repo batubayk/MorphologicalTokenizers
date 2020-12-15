a841dda5f07396a60f5034e0c2e5bfcb20ff96ff
Batuhan Baykara <batuhanbaykara@yahoo.com>
2020-12-15 12:17:18 +0300

    Squashed 'emmorphpy/' content from commit 8216538
    git-subtree-dir: emmorphpy
    git-subtree-split: 8216538620835a27c998333ef4fcbc8c7594f122

---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8, vim: expandtab:ts=4 -*-
+
+import sys
+import setuptools
+import importlib.util
+
+
+def import_pyhton_file(module_name, file_path):
+    # Import module from file: https://docs.python.org/3/library/importlib.html#importing-a-source-file-directly
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)
+    return module
+
+
+with open('README.md') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='emmorphpy',
+    # Get version without actually importing the module (else we need the dependencies installed)
+    version=getattr(import_pyhton_file('version', 'emmorphpy/version.py'), '__version__'),
+    author='dlazesz',  # Will warn about missing e-mail
+    description='A wrapper, a lemmatizer and REST API implemented in Python for emMorph (Humor)'
+                ' Hungarian morphological analyzer',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/dlt-rilmta/emmorphpy',
+    # license='GNU Lesser General Public License v3 (LGPLv3)',  # Never really used in favour of classifiers
+    # platforms='any',  # Never really used in favour of classifiers
+    packages=setuptools.find_packages(exclude=['tests']),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Operating System :: POSIX :: Linux',
+    ],
+    python_requires='>=3.6',
+    install_requires=['xtsv>=1.0.0,<2.0.0',
+                      'jprops',
+                      ],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'emmorphpy=emmorphpy.__main__:main',
+        ]
+    },
+)
